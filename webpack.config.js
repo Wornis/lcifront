9574bf78669bed8a194b8cfae4edb6bff1f03965
Kéviinraj BALARATNAM <keviinraj.balaratnam@gmail.com>
2019-04-08 00:28:39 +0200

    Basic structure ReactJs

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const {resolve} = require('path');
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: resolve(__dirname, 'build'),
+        filename: 'build.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: resolve(__dirname, 'public', 'index.html'),
+            filename: "./index.html"
+        })
+    ]
+};
