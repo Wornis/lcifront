5cf46d8a04efe9e50df463a103b9b4d2d252b9e4
Kéviinraj BALARATNAM <keviinraj.balaratnam@gmail.com>
2019-04-08 02:14:10 +0200

    add react-hot-loader |FINISH STRUCTURE BASE REACT

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,9 @@ module.exports = {
         })
     ], 
     resolve: {
-        extensions: ['.js', '.jsx', '.css']
+        extensions: ['.js', '.jsx', '.css'],
+        alias: {
+            'react-dom': '@hot-loader/react-dom'
+        }
     }
 };
